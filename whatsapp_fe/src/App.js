ba62c0a2bd0fe0f6cdd54035c641701dfee11334
paras <parasjuly2003@gmail.com>
2024-08-09 12:09:25 +0530

    Setting up Frontend

---
+++ whatsapp_fe/src/App.js
@@ -0,0 +1,12 @@
+import { ArrowIcon, ChatIcon } from "./svg";
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Welcome to the Frontend</h1>
+      <ChatIcon className="" />
+    </div>
+  );
+}
+
+export default App;
