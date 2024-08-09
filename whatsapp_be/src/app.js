c7a6b5f9b0f4ae932c3b4133aec126453d86ded0
paras <parasjuly2003@gmail.com>
2024-08-09 10:48:32 +0530

    add centralized error handling and graceful shutdown

--- whatsapp_be/src/app.js
+++ whatsapp_be/src/app.js
@@ -7,6 +7,8 @@ import cookieParser from "cookie-parser";
 import compression from "compression";
 import fileUpload from "express-fileupload";
 import cors from "cors";
+import httpErrors from "http-errors";
+import createHttpError from "http-errors";
 
 //dotenv config
 dotenv.config();
@@ -52,7 +54,25 @@ app.use(
 );
 
 app.post("/test", (req, res) => {
-  res.send("Hello from server");
+  throw createHttpError.BadRequest("this route has an error");
+});
+
+//route not found
+app.use(async (req, res, next) => {
+  //Creates a 404 Not Found error and passes it to the error handling middleware
+  next(createHttpError.NotFound("This route does not exist."));
+});
+
+//catches all errors passed through the `next()` function
+app.use(async (err, req, res, next) => {
+  // Sets the HTTP status code based on the error's status or defaults to 500 (Internal Server Error)
+  res.status(err.status || 500);
+  res.send({
+    error: {
+      status: err.status || 500,
+      message: err.message,
+    },
+  });
 });
 
 export default app;
--- whatsapp_be/src/index.js
+++ whatsapp_be/src/index.js
@@ -1,13 +1,36 @@
 import app from "./app.js";
-import logger from "./configs/logger.config.js"
+import logger from "./configs/logger.config.js";
 
-
-//env variables
 const PORT = process.env.PORT || 8000;
 
-
-
-app.listen(PORT, () => {
+let server = app.listen(PORT, () => {
   logger.info(`server running at port ${PORT}...`);
 });
 
+// Function to handle graceful server shutdown
+const exitHandler = () => {
+  if (server) {
+    logger.info("Server closed.");
+    process.exit(1);
+  } else {
+    process.exit(1);
+  }
+};
+
+// Function to handle unexpected errors (e.g., uncaught exceptions)
+const unexpectedErrorHandler = (error) => {
+  logger.error(error);
+  exitHandler();
+};
+
+// Handle uncaught exceptions and unhandled promise rejections
+process.on("uncaughtException", unexpectedErrorHandler);
+process.on("unhandledRejection", unexpectedErrorHandler);
+
+// Handle SIGTERM signal for graceful shutdown (e.g., when stopping the process)
+process.on("SIGTERM", () => {
+  if (server) {
+    logger.info("Server closed.");
+    process.exit(1);
+  }
+});
