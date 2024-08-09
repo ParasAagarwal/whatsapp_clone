08d4d28042271b7601b805fe12aad506d51020fe
paras <parasjuly2003@gmail.com>
2024-08-09 14:49:13 +0530

    Creating page skeletons and redux toolkit structure

--- whatsapp_fe/src/App.js
+++ whatsapp_fe/src/App.js
@@ -1,5 +1,22 @@
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+
+//pages
+import Home from "./pages/home";
+import Login from "./pages/login";
+import Register from "./pages/register";
+
 function App() {
-  return <div className="dark">Hi</div>;
+  return (
+    <div className="dark">
+      <Router>
+        <Routes>
+          <Route exact path="/" element={<Home />} />
+          <Route exact path="/login" element={<Login />} />
+          <Route exact path="/register" element={<Register />} />
+        </Routes>
+      </Router>
+    </div>
+  );
 }
 
 export default App;
---
+++ whatsapp_fe/src/app/store.js
@@ -0,0 +1,38 @@
+import { combineReducers, configureStore } from "@reduxjs/toolkit";
+import { persistReducer, persistStore } from "redux-persist";
+import createFilter from "redux-persist-transform-filter";
+import storage from "redux-persist/lib/storage";
+// Slices
+import userSlice from "../features/userSlice";
+
+// Filter to persist only the 'user' field from the 'user' slice
+const saveUserOnlyFilter = createFilter("user", ["user"]);
+
+// Configuration for redux-persist
+const persistConfig = {
+  key: "user", // Key for storing persisted data
+  storage, // Storage mechanism (localStorage in this case)
+  whitelist: ["user"], // Specify which slices to persist
+  transforms: [saveUserOnlyFilter], // Apply filter to save only certain fields
+};
+
+// Combine reducers (currently only 'userSlice')
+const rootReducer = combineReducers({
+  user: userSlice,
+});
+
+// Apply persistence to the root reducer
+const persistedReducer = persistReducer(persistConfig, rootReducer);
+
+// Create the Redux store with the persisted reducer
+export const store = configureStore({
+  reducer: persistedReducer,
+  middleware: (getDefaultMiddleware) =>
+    getDefaultMiddleware({
+      serializableCheck: false, // Disable certain Redux Toolkit checks for serializable actions
+    }),
+  devTools: true, // Enable Redux DevTools
+});
+
+// Create the persistor to handle rehydration
+export const persister = persistStore(store);
