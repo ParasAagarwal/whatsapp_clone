c06f22ec6fd4450aeb8956ea1fc110da3e767aa5
paras <parasjuly2003@gmail.com>
2024-08-15 10:46:37 +0530

    doc file attachment

--- whatsapp_fe/src/components/Chat/actions/attachments/menu/PhotoAttachments.jsx
+++ whatsapp_fe/src/components/Chat/actions/attachments/menu/PhotoAttachments.jsx
@@ -2,30 +2,38 @@ import { useDispatch } from "react-redux";
 import { PhotoIcon } from "../../../../../svg";
 import { useRef } from "react";
 import { addFiles } from "../../../../../features/chatSlice";
+import { getFileType } from "../../../../../utils/file";
 
 export default function PhotoAttachments() {
   const dispatch = useDispatch();
   const inputRef = useRef(null);
   const imageHandler = (e) => {
     let files = Array.from(e.target.files);
-    files.forEach((img) => {
+    files.forEach((file) => {
       if (
-        img.type !== "image/png" &&
-        img.type !== "image/jpeg" &&
-        img.type !== "image/gif" &&
-        img.type !== "image/webp"
+        file.type !== "image/png" &&
+        file.type !== "image/jpeg" &&
+        file.type !== "image/gif" &&
+        file.type !== "image/webp" &&
+        file.type !== "video/mp4" &&
+        file.type !== "video/mpeg" &&
+        file.type !== "video/webm"
       ) {
-        files.filter((item) => item.name !== img.name);
+        files.filter((item) => item.name !== file.name);
         return;
-      } else if (img.size > 1024 * 1024 * 5) {
-        files = files.filter((item) => item.name !== img.name);
+      } else if (file.size > 1024 * 1024 * 5) {
+        files = files.filter((item) => item.name !== file.name);
         return;
       } else {
         const reader = new FileReader();
-        reader.readAsDataURL(img);
+        reader.readAsDataURL(file);
         reader.onload = (e) => {
           dispatch(
-            addFiles({ file: img, imgData: e.target.result, type: "image" })
+            addFiles({
+              file: file,
+              fileData: e.target.result,
+              type: getFileType(file.type),
+            })
           );
         };
       }
